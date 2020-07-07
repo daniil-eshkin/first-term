e0daeb340d838bcf89b12de6e3eed89afcfb893b
daniil-eshkin <dan.eshkin@gmail.com>
2020-07-07 20:48:31 +0300

    HW3 minor fix

--- bigint-optimized/dynamic_buffer.cpp
+++ bigint-optimized/dynamic_buffer.cpp
@@ -1,7 +1,7 @@
 #include "dynamic_buffer.h"
 
 static uint32_t *allocate(size_t size) {
-    return (size == 0 ? nullptr : reinterpret_cast<uint32_t*>(operator new(size * sizeof(uint32_t))));
+    return (size == 0 ? nullptr : static_cast<uint32_t*>(operator new(size * sizeof(uint32_t))));
 }
 
 dynamic_buffer::dynamic_buffer(size_t size)
